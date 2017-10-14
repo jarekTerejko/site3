c2a73b4c2a5224abdfa3bac6a0705f0f5a0b87be
Jarosław Terejko <j.terejko@gmail.com>
2017-10-14 22:55:17 +0200

    added grunt and tasks

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    
+      // Project configuration.
+      grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'sass/style.sass'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+    
+      });
+      // Load the plugins tasks
+      grunt.loadNpmTasks('grunt-sass');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-browser-sync');
+      
+      
+      
+      // Default task(s).
+      grunt.registerTask('default', ['browserSync', 'watch' ]);
+    };
